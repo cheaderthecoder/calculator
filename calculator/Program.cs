1aa3a77d01023e716c829e84688556f8700b64b0
cheaderthecoder <zeinosman23@gmail.com>
2024-09-01 17:25:45 +0300

    Cleaner code

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,27 +1,47 @@
-﻿internal class Program
+﻿using System.Formats.Asn1;
+
+namespace Calculator.CLI;
+internal class ConsoleCalculator
 {
+    public static int num1 = 0;
+    public static int num2 = 0;
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Click enter to Start");
-        string anser = Console.ReadLine();
-        anser.ToUpper();
-        while (anser == "" +"" || anser == "Y")
+
+        string Answer = Console.ReadLine()?.Trim();
+        Answer.ToUpper();
+        
+        while (Answer != "N")
         {
             Console.Clear();
-            Console.WriteLine("Welcome Enter in What You Want to do\n");
-            Console.WriteLine("enter + for Addition\n");
-            Console.WriteLine("enter - for Subtraction\n");
-            Console.WriteLine("enter / for Division\n");
-            Console.WriteLine("enter * for multiplication\n");
-            Console.WriteLine("enter x for multiplication\n");
-            Console.WriteLine("");
+            Console.WriteLine
+            (@"
+            Welcome Enter in What You Want to do
+            enter + for Addition
+            enter - for Subtraction
+            enter / for Division
+            enter * for multiplication
+            enter x for multiplication
+            ");
 
-            string ans = Console.ReadLine();
-            Console.WriteLine("Write 2 numbers");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int num3;
-            switch (ans)
+            string Oporation = Console.ReadLine()?.Trim();
+            Console.WriteLine("Write your Equation...");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int num3 = GetValue(Oporation, num1, num2);
+
+            Console.WriteLine("do you want to continue (Y/N)");
+            Answer = Console.ReadLine();
+        }
+
+        static int GetValue(string Oporation, int num1, int num2)
+        {
+            int num3 = 0;
+            switch (Oporation)
             {
                 case "+":
                     num3 = num1 + num2;
@@ -46,11 +66,9 @@
                     num3 = num1 / num2;
                     Console.WriteLine("\n" + num3 + "\n");
                     break;
-
             }
-            ans = "";
-            Console.WriteLine("do you want to continue (Y/N)");
-            anser = Console.ReadLine();
+
+            return num3;
         }
     }
 }
